2a927b38cfbc63f6ad4fac47cda88cd4502aaf04
lukgru <lukasz.gruchala@fitech.pl>
2025-10-10 21:00:46 +0200

    created home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [NgStyle],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
