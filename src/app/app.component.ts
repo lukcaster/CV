c9d93d264cd8672444e5911314a0d99abb579759
lukgru <lukasz.gruchala@fitech.pl>
2025-10-10 21:01:22 +0200

    made some changes in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HomeComponent, AboutComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CV';
+
+  ngOnInit(): void {
+    AOS.init()
+  }
 }
